593ce4d5a8c22015515dbc27f0da69e8d564bb49
uncleboss12 <justineobiazi@gmail.com>
2023-05-08 10:16:24 -0700

    Another correction to the removal of the feedback panel

--- interactwel/src/components/dashboard/projects/wizard/Visualization.vue
+++ interactwel/src/components/dashboard/projects/wizard/Visualization.vue
@@ -1,65 +1,29 @@
 <template>
-  <div class="container-fluid">
-    <b-row>
-      <b-col>
-        <h6 class="mt-3">
-          <div class="title">
-            <b-nav pills>
-              <b-nav-item>Adaptation Plan {{ $route.params.planId }}</b-nav-item>
-            </b-nav>
-          </div>
-        </h6>
-        <router-view :key="$route.fullPath" />
-      </b-col>
-    </b-row>
+  <div>
+    <h6 class="mt-3">
+      Adaptation Plan {{ $route.params.planId }}
+    </h6>
+    <router-view :key="$route.fullPath" />/>
   </div>
 </template>
 
-<script>
-// import FeedbackPage from '@/components/dashboard/projects/charts/Feedback.vue';
 
-// export default {
-//   name: "Visualization",
-//   components: {
-//     FeedbackPage,
-//   },
-//   data() {
-//     return {
-//       showSidebar: false,
-//     };
-//   },
-// };
+<script>
+export default {
+  name: 'Visualization',
+  props: {},
+  data() {
+    return {};
+  },
+  watch: {},
+  mounted() {
+  },
+  methods: {},
+};
 </script>
 
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.container-fluid {
-  padding: 0;
-}
-
-.title {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.sidebar-container {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  z-index: 1000;
-}
-
-.sidebar-wrapper {
-  display: flex;
-  flex-direction: row;
-  height: 100%;
-}
-
-.sidebar-content {
-  width: 100%;
-  padding: 15px;
-  flex: 1;
-}
 </style>
+
