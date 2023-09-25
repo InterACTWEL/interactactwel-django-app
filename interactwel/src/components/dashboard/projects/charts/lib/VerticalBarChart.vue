8324d026dde9335f9e6585d8e13468792d25b3a9
uncleboss12 <justineobiazi@gmail.com>
2023-09-24 17:07:34 -0700

    the code that was on my local that i deleted, commit to the new local.

---
+++ interactwel/src/components/dashboard/projects/charts/lib/VerticalBarChart.vue
@@ -0,0 +1,50 @@
+<template>
+  <Bar
+    v-if="chartData != null"
+    :chart-data="chartData"
+    :chart-options="options"
+  />
+</template>
+
+<script>
+
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+export default {
+  name: 'VerticalBarChart',
+  components: { Bar },
+  props: {
+    chartData: {
+      type: Object,
+      required: false,
+    },
+    options: {},
+  },
+};
+
+</script>
+
+<!--import { Bar, mixins } from 'vue-chartjs';
+const { reactiveProp } = mixins;
+
+export default {
+  extends: Bar,
+  mixins: [reactiveProp],
+
+  props: {
+    chartData: {
+      type: Array | Object,
+      required: false,
+    },
+    options: {},
+  },
+
+  mounted() {
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.chartData, this.options);
+  },
+};-->
