08f910899407d17a23fa1b2465e2bdd1af7bf1da
uncleboss12 <justineobiazi@gmail.com>
2023-11-22 09:23:55 -0800

    better changes done to the rangeline generalised plot

--- interactwel/src/components/dashboard/projects/charts/data/RangeAreaAndLineChart.vue
+++ interactwel/src/components/dashboard/projects/charts/data/RangeAreaAndLineChart.vue
@@ -1,111 +1,62 @@
-<template>
-  <CanvasJSChart :options="options" 
-  :style="styleOptions" 
-  @chart-ref="chartInstance" />
-</template>
-
 <script>
 import CanvasJSChart from '@/components/CanvasJSVueComponent.vue';
-import axios from 'axios';
 
 export default {
-  name: "TemperatureChart",
   components: {
-    CanvasJSChart,
+    CanvasJSChart
   },
   data() {
     return {
       chart: null,
       options: {
-        exportEnabled: true,
         title: {
-          text: "Temperature Variation",
+          text: "RangeArea Chart With Average Line Series"
         },
-        subtitles: [{
-          text: "California, USA",
-        }],
-        axisY: {
-          title: "Temperature (°C)",
-          suffix: "°C",
-        },
-        axisX: {
-          interval: 1,
-          intervalType: "month",
-          valueFormatString: "MMM",
-        },
-        toolTip: {
-          shared: true,
-        },
-        legend: {
-          cursor: "pointer",
-          itemclick: this.toggleDataSeries,
-        },
-        data: [{
-          type: "rangeArea",
-          showInLegend: true,
-          name: "Temperature Range",
-          yValueFormatString: "##.0 °C",
-          xValueFormatString: "MMMM YYYY",
-          toolTipContent: "{x}<br><span style='color:{color}'>Min</span>: {y[0]}<br><span style='color:{color}'>Max</span>: {y[1]}",
-          xValueType: "dateTime",
-          markerSize: 0,
-          dataPoints: []
-        }],
+        data: [
+          {
+            type: "rangeArea",
+            dataPoints: []
+          },
+          {
+            type: "line",
+            dataPoints: []
+          }
+        ]
       },
       styleOptions: {
         width: "100%",
-        height: "360px",
-      },
-    };
+        height: "360px"
+      }
+    }
   },
   mounted() {
-    axios.get("/static/stream-temperature-data.json").then(response => {
-      const dataPoints = response.data.map(item => ({
-        x: new Date(item.date),
-        y: [item.min, item.max],
-        Average: (item.min + item.max) / 2, // Added the Average key to the dataPoints array
-      }));
-      this.options.data[0].dataPoints = dataPoints; // Added the Average key to the options.data[0].dataPoints array
-      this.chart.render();
-    });
-  },
-  methods: {
-    toggleDataSeries(e) {
-      if (typeof e.dataSeries.visible === "undefined" || e.dataSeries.visible) {
-        e.dataSeries.visible = false;
-      } else {
-        e.dataSeries.visible = true;
-      }
-      e.chart.render();
-    },
-    addAverageSeries() {
-      let dps = [];
-      let chart = this.chart;
-      for (let i = 0; i < chart.options.data[0].dataPoints.length; i++) {
-        const dataPoint = chart.options.data[0].dataPoints[i];
-        dps.push({
-          x: dataPoint.x,
-          y: dataPoint.Average, // Use the "Average" value from the JSON
-        });
-      }
-      chart.options.data.push({
-        type: "line",
-        name: "Average",
-        showInLegend: true,
-        visible: true, // Added the visible: true property
-        markerType: "triangle",
-        markerSize: 0,
-        color: "blue",
-        yValueFormatString: "##.0 °C",
-        toolTipContent: "<span style='color:{color}'>{name}</span>: {y}",
-        dataPoints: dps,
-      });
-      chart.render();
-    },
-    chartInstance(chart) {
-      this.chart = chart;
-      this.addAverageSeries();
-    },
-  },
-};
+    var dataPoints = [
+    { x: new Date(2019, 0, 1), y: [-22.4332, 1.701, 26.701] },
+              { x: new Date(2019, 1, 1), y: [-22.8482, 6.701, 29.9354] },
+              { x: new Date(2019, 2, 1), y: [-16.5272, 9.701, 33.1793] },
+              { x: new Date(2019, 3, 1), y: [-10.3894, 11.701, 39.6753] },
+              { x: new Date(2019, 4, 1), y: [-13.1862, 13.701, 38.289] },
+              { x: new Date(2019, 5, 1), y: [-2.9892, 15.701, 46.6849] },
+              { x: new Date(2019, 6, 1), y: [2.20233, 16.701, 47.2412] },
+              { x: new Date(2019, 7, 1), y: [0.975366, 15.701, 47.4759] },
+              { x: new Date(2019, 8, 1), y: [-6.26288, 18.701, 46.0959] },
+              { x: new Date(2019, 9, 1), y: [-9.87604, 20.701, 37.9325] },
+              { x: new Date(2019, 10, 1), y: [-21.6, 14.701, 33.2763] },
+              { x: new Date(2019, 11, 1), y: [-19.3211, 3.701, 27.0527] }
+    ];
+    var dps1 = [], dps2 = [];
+
+    for(var i = 0; i < dataPoints.length; i++) {
+      dps1.push({x: dataPoints[i].x, y:[dataPoints[i].y[0], dataPoints[i].y[2]]});
+      dps2.push({x: dataPoints[i].x, y:dataPoints[i].y[1]});
+    }
+
+    this.options.data[0].dataPoints = dps1;
+    this.options.data[1].dataPoints = dps2;
+  }
+}
 </script>
+
+<template>
+  <CanvasJSChart :options="options" :style="styleOptions"/>
+</template>
